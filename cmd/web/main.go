5d9d82fd7afd2860306e57702cae381936099d63
Nick Clyde <nick@clyde.tech>
2024-07-12 22:35:09 -0700

    Chapter 2.8

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
 func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("GET /{$}", home)
-    mux.HandleFunc("GET /snippet/view/{id}", snippetView)
-    mux.HandleFunc("GET /snippet/create", snippetCreate)
-    mux.HandleFunc("POST /snippet/create", snippetCreatePost)
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-    log.Print("starting server on :4000")
-    
-    err := http.ListenAndServe("localhost:4000", mux)
-    log.Fatal(err)
+	log.Print("starting server on :4000")
+
+	err := http.ListenAndServe("localhost:4000", mux)
+	log.Fatal(err)
 }
