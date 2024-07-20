5f698d3855af2cb10f7c0e694e7d24ca9be9b2ef
Nick Clyde <nick@clyde.tech>
2024-07-20 12:13:22 -0700

    Chapter 5.2

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,7 +1,16 @@
 package main
 
 import "github.com/nickclyde/snippetbox/internal/models"
+import (
+	"path/filepath"
+	"text/template"
+
+	"github.com/nickclyde/snippetbox/internal/models"
+)
 
 type templateData struct {
 	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
+}
 }
