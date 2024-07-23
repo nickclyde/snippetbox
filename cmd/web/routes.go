b1b0255a6bf09478143bd0ea428d4161f9cf0917
Nick Clyde <nick@clyde.tech>
2024-07-23 15:56:29 -0700

    Chapter 10.1

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -6,7 +6,6 @@ import (
 	"github.com/justinas/alice"
 )
 
-// The routes() method returns a servemux containing our application routes.
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
@@ -19,6 +18,12 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
 	mux.Handle("GET /snippet/create", dynamic.ThenFunc(app.snippetCreate))
 	mux.Handle("POST /snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
+
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
+	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
 
 	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
 
